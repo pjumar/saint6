34722f99bccdfe23e37a8ed2ea115826a08737d3
Pho Huynh <p@ccly.dev>
2026-02-09 17:12:16 +0700

    fix: update strapi default email from address

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -2,7 +2,7 @@ export default ({ env }) => ({
   email: {
     config: {
       settings: {
-        defaultFrom: env("EMAIL_FROM", "noreply@strapi.cloud"),
+        defaultFrom: env("EMAIL_FROM", "noreply@saint6.studio"),
         defaultReplyTo: env("EMAIL_REPLY_TO", "saint6studios@gmail.com"),
       },
     },
